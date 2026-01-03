cc536ebf5c62acbbbb96c03cb9f9b92fed4648c2
DheebhanJeganathan06 <dheebhanj06@gmail.com>
2026-01-03 09:35:03 -0500

    state button functionality + reset/option buttons created

--- js/script.js
+++ js/script.js
@@ -1,20 +1,13 @@
-const toggleButton = document.getElementById('themeToggle');
-const root = document.documentElement;
+let startBtn = document.getElementById("start-btn");
+let resetBtn = document.getElementById("reset-btn");
+let optionsBtn = document.getElementById("options-btn");
+let timerText = document.getElementById("timer-txt");
+let quoteText = document.getElementById("quote-txt");
 
-// Load saved theme on page load
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme === 'dark') {
-  root.classList.add('dark');
-}
-
-// Toggle theme on click
-toggleButton.addEventListener('click', () => {
-  root.classList.toggle('dark');
-
-  // Save preference
-  if (root.classList.contains('dark')) {
-    localStorage.setItem('theme', 'dark');
-  } else {
-    localStorage.setItem('theme', 'light');
-  }
-});
+const stateButtons = document.querySelectorAll(".mode-btn");
+  stateButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+      stateButtons.forEach(b => b.classList.remove("selected"));
+      btn.classList.add("selected");
+    });
+  });
